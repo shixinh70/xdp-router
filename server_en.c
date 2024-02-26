78056e72b3f23b58389a5f11b22c573efb47d1b7
shixinh70 <shixinh70@gmail.com>
2024-02-27 03:33:01 +0800

    re compute tcp csum

--- server_en.c
+++ server_en.c
@@ -120,7 +120,7 @@ SEC("prog") int xdp_router(struct __sk_buff *skb) {
                     ip->saddr ^= ip->daddr;
 
                     // Swap port and convert to ack packet
-                    __u64 tcp_csum = tcp->check;
+                    //__u64 tcp_csum = tcp->check;
                     __u32* ptr ; 
                     ptr = ((void*)tcp) + 12;
                     if((void*)ptr + 4 > data_end) return TC_ACT_SHOT;
@@ -139,10 +139,14 @@ SEC("prog") int xdp_router(struct __sk_buff *skb) {
                     tcp->ack_seq = rx_seg + bpf_htonl(1);
                     DEBUG_PRINT("TC:ACK seg = %u, ack = %u\n",bpf_ntohl(tcp->seq), bpf_ntohl(tcp->ack_seq));
 
-                    tcp_csum = bpf_csum_diff(&tcp_old_flag, 4, &tcp_new_flag, 4, ~tcp_csum);
-                    tcp_csum = bpf_csum_diff(&rx_seg, 4, &tcp->ack_seq, 4, tcp_csum);
-                    tcp->check = csum_fold_helper_64(tcp_csum) ;
-                    
+                    // tcp_csum = bpf_csum_diff(&tcp_old_flag, 4, &tcp_new_flag, 4, ~tcp_csum);
+                    // tcp_csum = bpf_csum_diff(&rx_seg, 4, &tcp->ack_seq, 4, tcp_csum);
+                    // tcp->check = csum_fold_helper_64(tcp_csum) ;
+                    tcp->check = 0;
+                    __u64 tcp_csum_tmp = 0;
+                    if(((void*)tcp)+ 36 > data_end) return XDP_DROP;
+                    ipv4_l4_csum(tcp, 36, &tcp_csum_tmp, ip); // Use fixed 36 bytes
+                    tcp->check = tcp_csum_tmp;
 
                     // Swap tsval and tsecr. Do we need to change the ts order to NOP NOP TS ?   
                     ts->tsval ^= ts->tsecr;
